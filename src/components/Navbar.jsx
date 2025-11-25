329231143a8b97ebcc6181aac5f1a8bacdce0e24
darius <dariusjackon11@gmail.com>
2025-11-25 22:16:03 +0300

    Update: Navbar without clear responsiveness.

--- src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-
-function Navbar(){
-    return(
-        <h1>Hey there! Our school’s new website has officially begun.</h1>
-    )
-}
-export default Navbar;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import Logo from "../../assets/logo.svg";
+import LangLogo from "../../assets/language.svg";
+import "./Navbar.css"
+import { useState } from "react";
+function Navbar(){
+    const [Open , setOpen] = useState(false);
+    return(
+        <nav className="navbar-container">
+           <div className="nav-items">
+             <a href="">
+                <span><img src={Logo} className="nav-logo"/></span>
+                <span className="logo-name">EduPortal</span>
+             </a>
+
+                <div className= {`nav-links ${Open ? "Open" : ""}`}>
+                    <ul className="navigation-links">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Academics</li>
+                        <li>Gallery</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+                <div className="nav-lang">
+                      <img src={LangLogo} className="langLogo" />
+                      <select onChange={(e) => console.log("Language changed to", e.target.value)}>
+                        <option value="en">English</option>
+                        <option value="fr">Français</option>
+                        <option value="es">Español</option>
+                        <option value="sw">Swahili</option>
+                      </select>
+                </div>
+                 <button className="nav-applyBtn">Apply Now</button>
+                  <div className="hamburger" onClick={() => setOpen(!Open)}>
+                    ☰
+                </div>
+           </div>
+        </nav>
+    )
+}
+export default Navbar;
